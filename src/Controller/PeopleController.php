55d8a77727118477ad24f5f99977372e87f37d84
Kegan VanSickle <keganv@keganv.com>
2018-04-17 20:08:27 -0500

    - Stubs for controller

--- src/Controller/PeopleController.php
+++ src/Controller/PeopleController.php
@@ -2,50 +2,15 @@
 
 namespace Controller;
 
-use Psr\Container\ContainerInterface;
-
-use Slim\Http\Request;
 use Slim\Http\Response;
 
-class PeopleController
+class PeopleController extends DefaultController
 {
-    /** @var ContainerInterface */
-    protected $container;
-
-    /** @var Request */
-    protected $request;
-
-    /** @var Response */
-    protected $response;
-
-    /** @var array */
-    protected $data = [];
-
     /**
-     * DefaultController constructor.
-     * @param ContainerInterface $container
-     * @param Request|null $request
-     * @param Response|null $response
-     * @param array $data
-     */
-    public function __construct(
-        ContainerInterface $container,
-        Request $request = null,
-        Response $response = null,
-        array $data = []
-    ) {
-        $this->container = $container;
-        $this->request = $request;
-        $this->response = $response;
-        $this->data = $data;
-    }
-
-    /**
-     * @return Response
-     */
+     * Stubs for People Controller
     public function index() {
         $data = array_merge($this->data, [
-            'layout' => 'index',
+            'layout' => 'person/index',
             'page_title' => 'People',
             'users' => $this->container->get('entity_manager')->getRepository('Entity\Person')->findAll(),
         ]);
@@ -61,11 +26,12 @@ class PeopleController
         }
 
         $data = array_merge($this->data, [
-            'layout' => 'view',
+            'layout' => 'person/view',
             'page_title' => 'Viewing: ' . $user->getFirstname(),
             'user' => $user,
         ]);
 
         return $this->container->get('renderer')->render($this->response, 'base.phtml', $data);
     }
+    */
 }
